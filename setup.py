8e42482a7701295bda7ecb1a473bb36f4a9bb4c3
antonyhclark <antony.clark2@nhs.scot>
2024-10-30 17:15:19 +0000

    further package set up

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setup import setup
+
+setup(name='my_package', # name of top level folder containing __init__.py
+      version='0.0.1',
+      description='Perform and visualize a text analysis.',
+      author='ahc',
+      # as name plus sub folders containing __init__.py files i.e. sub-packages
+      packages=['my_package', 'sub_package'])
